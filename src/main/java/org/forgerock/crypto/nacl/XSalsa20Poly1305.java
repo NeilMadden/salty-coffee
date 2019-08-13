2c451617d6c7786068f4c1c21fc170fd08b0aede
Neil Madden <neil.madden@forgerock.com>
2019-08-13 16:17:33 +0100

    Correct copyright headers.

--- src/main/java/org/forgerock/crypto/nacl/XSalsa20Poly1305.java
+++ src/main/java/org/forgerock/crypto/nacl/XSalsa20Poly1305.java
@@ -1,9 +1,17 @@
 /*
- * Copyright 2019 ForgeRock AS. All Rights Reserved
+ * Copyright 2019 Neil Madden.
  *
- * Use of this code requires a commercial software license with ForgeRock AS.
- * or with one of its affiliates. All use shall be exclusively subject
- * to such license between the licensee and ForgeRock AS.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package org.forgerock.crypto.nacl;
